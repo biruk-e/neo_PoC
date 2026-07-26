f074f3d583b8477fdf5f3df4ed4bfce29a8c813b
biruk-e <eliasbiruk.no@gmail.com>
2026-07-26 00:01:52 -0400

    feat(joystick): implement core driver API, DMA init, and axis normalization math

---
+++ Core/Src/joystick.c
@@ -0,0 +1,116 @@
+#include "joystick.h"
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * @brief Initializes a joystick instance, binds hardware handle,
+ * initializes joystick parameters and dma buffer, and starts DMA sampling
+ * @param joystick: pointer to an instance of Joystick_t
+ * @param hadc: pointer to the ADC handle
+ * @retval HAL_StatusTypeDef: HAL Status of the DMA start operation
+ */
+HAL_StatusTypeDef Joystick_Init(Joystick_t *joystick, ADC_HandleTypeDef *hadc) {
+	// returns an error flag if either Joystick_t or ADC_HandleTypeDef handles have not been initialized in main
+	if (joystick == NULL || hadc == NULL) {
+		return HAL_ERROR;
+	}
+
+	joystick->hadc = hadc; // bind hardware handle
+
+	// set default midpoints and deadzones
+	joystick->calib.center_x = JOYSTICK_DEFAULT_MIDPOINT;
+	joystick->calib.center_y = JOYSTICK_DEFAULT_MIDPOINT;
+	joystick->calib.deadzone = JOYSTICK_DEFAULT_DEADZONE;
+
+	// initialize x_norm and y_norm to 0.0
+	joystick->x_norm = 0.0f;
+	joystick->y_norm = 0.0f;
+
+	// initialize x_raw and y_raw to 0
+	joystick->x_raw = 0;
+	joystick->y_raw = 0;
+
+	// initialize dma_buffer[0] and dma_buffer[1] to 0
+	joystick->raw_dma[0] = 0;
+	joystick->raw_dma[1] = 0;
+
+	// start ADC conversion in DMA mode and return HAL status
+	return HAL_ADC_Start_DMA(joystick->hadc, (uint32_t*)joystick->raw_dma, JOYSTICK_ADC_CHANNELS);
+}
+
+/**
+ * @brief Samples resting ADC readings to set dynamic zero points for x and y axes
+ * @param joystick: pointer to an instance of Joystick_t
+ */
+void Joystick_Calibrate(Joystick_t *joystick) {
+	// immediately returns if joystick pointer is NULL
+	if (joystick == NULL) {
+		return;
+	}
+	// sample current ADC readings and set as the center for each axis (VRy = x, and VRx = y for current orientation of breakout)
+	joystick->calib.center_x = joystick->raw_dma[1];
+	joystick->calib.center_y = joystick->raw_dma[0];
+}
+
+/**
+ * @brief Reads latest DMA values, calculates axis delta, applies deadzone, and normalizes output to [-1.0f, +1.0f]
+ * @param joystick: pointer to an instance of Joystick_t
+ */
+void Joystick_Update(Joystick_t *joystick) {
+	// immediately returns if joystick pointer is NULL
+		if (joystick == NULL) {
+			return;
+		}
+
+	// fetch raw values from DMA buffer (VRy = x, and VRx = y for current orientation of breakout)
+	joystick->x_raw = joystick->raw_dma[1];
+	joystick->y_raw = joystick->raw_dma[0];
+
+	// calculate change in x and y from resting center
+	int32_t delta_x = (int32_t)joystick->x_raw - (int32_t)joystick->calib.center_x; // delta_x = x_raw - center_x
+	int32_t delta_y = (int32_t)joystick->y_raw - (int32_t)joystick->calib.center_y; // delta_y = y_raw - center_y
+
+	// x-axis normalization
+	if (abs(delta_x) <= joystick->calib.deadzone) {
+		joystick->x_norm = 0.0f;
+	} else {
+		if (delta_x > 0) { /*pushing right*/
+			float effective_delta_x = (float)delta_x - (float)joystick->calib.deadzone; // effective_delta_x = delta_x - deadzone
+			float max_span = JOYSTICK_ADC_MAX_RAW - (float)joystick->calib.center_x - (float)joystick->calib.deadzone; // max_span = 4095 - center_x - deadzone
+			joystick->x_norm = effective_delta_x / max_span; // normalized x (can be [-1.0 to +1.0] in general)
+		} else { /*pushing left*/
+			float effective_delta_x = (float)delta_x + (float)joystick->calib.deadzone; // effective_delta_x = delta_x + deadzone
+			float min_span = (float)joystick->calib.center_x - (float)joystick->calib.deadzone; // min_span = center_x - deadzone
+			joystick->x_norm = effective_delta_x / min_span; // normalized x (can be [-1.0 to +1.0] in general)
+		}
+	}
+
+	// x-axis normalization
+		if (abs(delta_y) <= joystick->calib.deadzone) {
+			joystick->y_norm = 0.0f;
+		} else {
+			if (delta_y > 0) { /*pushing right*/
+				float effective_delta_y = (float)delta_y - (float)joystick->calib.deadzone; // effective_delta_y = delta_y - deadzone
+				float max_span = JOYSTICK_ADC_MAX_RAW - (float)joystick->calib.center_y - (float)joystick->calib.deadzone; // max_span = 4095 - center_y - deadzone
+				joystick->y_norm = effective_delta_y / max_span; // normalized y (can be [-1.0 to +1.0] in general)
+			} else { /*pushing left*/
+				float effective_delta_y = (float)delta_y + (float)joystick->calib.deadzone; // effective_delta_y = delta_y + deadzone
+				float min_span = (float)joystick->calib.center_y - (float)joystick->calib.deadzone; // min_span = center_y - deadzone
+				joystick->y_norm = effective_delta_y / min_span; // normalized y (can be [-1.0 to +1.0] in general)
+			}
+		}
+
+		// clamp x_norm and y_norm to be within -1.0 to +1.0 (accounts for slightly-off center midpoint during calibration)
+		if (joystick->x_norm > 1.0f) {
+			joystick->x_norm = 1.0f;
+		} else if (joystick->x_norm < -1.0f) {
+			joystick->x_norm = -1.0f;
+		}
+
+		if (joystick->y_norm > 1.0f) {
+			joystick->y_norm = 1.0f;
+		} else if (joystick->y_norm < -1.0f) {
+			joystick->y_norm = -1.0f;
+		}
+
+}
