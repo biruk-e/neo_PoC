e906ab9f626d4c28e4e7499c002a5cb86eb29525
biruk-e <eliasbiruk.no@gmail.com>
2026-07-25 20:32:54 -0400

    added a JOYSTICK_DEFAULT_MIDPOINT definition

--- Core/Inc/joystick.h
+++ Core/Inc/joystick.h
@@ -9,6 +9,7 @@ extern "C" {
 #define JOYSTICK_ADC_CHANNELS 2 // we are reading from 2 adc channels
 #define JOYSTICK_ADC_MAX_RAW 4095.0f // can be used for conversion math later on (float to prevent unwanted rounding down)
 #define JOYSTICK_DEFAULT_DEADZONE 150 // band in adc readings in which any fluctuation is ignored
+#define JOYSTICK_DEFAULT_MIDPOINT 2047 // default midpoint of adc range (4095/2)
 
 // typedef struct for holding calibration values
 typedef struct {
@@ -18,7 +19,6 @@ typedef struct {
 } Joystick_Calib_t;
 
 // typedef struct that represents the joystick and its state
-
 typedef struct {
 	ADC_HandleTypeDef *hadc; // pointer to ADC peripheral
 	volatile uint16_t raw_dma[JOYSTICK_ADC_CHANNELS]; // target DMA buffer
@@ -32,7 +32,7 @@ typedef struct {
 	// raw ADC values that can be accessed if needed for debugging
 	uint16_t x_raw;
 	uint16_t y_raw;
-}Joystick_t;
+} Joystick_t;
 
 // public API
 HAL_StatusTypeDef Joystick_Init(Joystick_t *joystick, ADC_HandleTypeDef *hadc);
